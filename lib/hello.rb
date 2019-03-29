a7448191bc47df1f2f330b751d059d55bb63e4f9
Angela Mumba <malaika.mumba@gmail.com>
2019-03-29 18:32:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,14 @@
 def hello_t(array)
-i=0 
+if i=0 
 while i < array.length
 yield array [i]
 i= i + 1 
 end 
 array
+else
+puts"Hey!No block was given"
+end 
 end
-
 # call your method here!
 ["Tim", "Tom", "Jim"]
 hello_t(["Tim", "Tom", "Jim"]) do |name|
