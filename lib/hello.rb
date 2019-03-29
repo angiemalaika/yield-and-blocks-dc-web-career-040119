d1d48bc8310abff4f6adaeef5b05dd827b54e584
Angela Mumba <malaika.mumba@gmail.com>
2019-03-29 18:34:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,9 +10,9 @@ puts"Hey!No block was given"
 end 
 end
 # call your method here!
-["Tim", "Tom", "Jim"]
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
- end 
-end
+#["Tim", "Tom", "Jim"]
+#hello_t(["Tim", "Tom", "Jim"]) do |name|
+#  if name.start_with?("T")
+ #   puts "Hi, #{name}"
+# end 
+#end
